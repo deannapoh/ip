bedc1eb9b19dbe2c5b41a8fe6f4f48591c76ad50
Deanna <e1157414@u.nus.edu>
2025-01-19 20:40:27 +0800

    added echo

--- src/main/java/Boo.java
+++ src/main/java/Boo.java
@@ -1,11 +1,40 @@
+import java.util.Scanner;
+
 public class Boo {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
+        String greeting = "____________________________________________________________\n"
                 + "Hello! I'm Boo \n"
                 + "What can I do for you? \n"
-                + "____________________________________________________________ \n"
-                + "Bye. Hope to see you again soon! \n"
                 + "____________________________________________________________ \n";
-        System.out.println(logo);
+        System.out.println(greeting);
+
+        // Create scanner to read user inputs
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            // Read user input
+            String input = scanner.nextLine();
+
+            // Exit and show goodbye message if user types "bye"
+            if (input.equalsIgnoreCase("bye")) {
+                String goodbyeMessage = "____________________________________________________________ \n"
+                        + "Bye. Hope to see you again soon! \n"
+                        + "____________________________________________________________ \n";
+
+                System.out.println(goodbyeMessage);
+                break;
+            }
+
+            // Else, repeat whatever user types
+            String echo = "____________________________________________________________ \n"
+                    + input + "\n"
+                    + "____________________________________________________________ \n";
+            System.out.println(echo);
+        }
+
+        scanner.close();
+
     }
+
+
 }
