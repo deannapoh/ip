4dcaff5d321805aa5fcc17919bf8e886bffce787
deannapoh <e1157414@u.nus.edu>
2025-01-19 19:04:11 +0800

    renamed chatbot and added greet and exit functionalities

---
+++ src/main/java/Boo.java
@@ -0,0 +1,11 @@
+public class Boo {
+    public static void main(String[] args) {
+        String logo = "____________________________________________________________\n"
+                + "Hello! I'm Boo \n"
+                + "What can I do for you? \n"
+                + "____________________________________________________________ \n"
+                + "Bye. Hope to see you again soon! \n"
+                + "____________________________________________________________ \n";
+        System.out.println(logo);
+    }
+}
