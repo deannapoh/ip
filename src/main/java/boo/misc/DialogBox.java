abda1c34cfe1a27ce6363561d07e546c0b24c7f2
Deanna <e1157414@u.nus.edu>
2025-02-21 18:50:02 +0800

    Improve GUI

--- src/main/java/boo/misc/DialogBox.java
+++ src/main/java/boo/misc/DialogBox.java
@@ -53,6 +53,7 @@ public class DialogBox extends HBox {
         Collections.reverse(tmp);
         getChildren().setAll(tmp);
         setAlignment(Pos.TOP_LEFT);
+        dialog.getStyleClass().add("reply-label");
     }
 
     /**
